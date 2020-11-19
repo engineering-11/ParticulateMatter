5507b65b1ca7153a945b8b2560657b61d8257d00
SpiderMonkey177 <70351379+SpiderMonkey177@users.noreply.github.com>
2020-11-18 22:02:55 -0800

    Update partmatter.py

--- partmatter.py
+++ partmatter.py
@@ -5,7 +5,7 @@ ser = serial.Serial('/dev/ttyUSB0')
 x=0
 while x < 1:
     data = ser.read(32)
-    extract =(data[5]<<8)+data[6]
+    extract =int.from_bytes(data[6:8],byteorder='big')
     print("PM 2.5 Concentration is",extract,"ug/m^3")
     time.sleep(5)
 
